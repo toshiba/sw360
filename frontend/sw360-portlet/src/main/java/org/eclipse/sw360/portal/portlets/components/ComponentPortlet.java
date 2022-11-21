5b3e4c27f93f0b63e1f966ab13efe5e51f2bc64e
tuannn2 <tuan2.nguyennhu@toshiba.co.jp>
2022-11-21 09:48:16 +0700

    edit tag index of package information

--- frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/portlets/components/ComponentPortlet.java
+++ frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/portlets/components/ComponentPortlet.java
@@ -1172,7 +1172,6 @@ public class ComponentPortlet extends FossologyAwarePortlet {
                     setPackage.add(packageInfoJson);
                     packageArray.put(packageInfoJson);
                 }
-                log.info("-----packageInfoJson--------"+setPackage.toString());
                 request.setAttribute("packageInfoJson", setPackage);
             } catch (JsonProcessingException e) {
                 log.error("Error when write Value As String PackageInformation ",e);
