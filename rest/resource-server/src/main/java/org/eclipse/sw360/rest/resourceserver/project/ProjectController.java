b885fc323f88cf1a0b95f7bb29f352587e471704
hoangnt2 <hoang2.nguyenthai@toshiba.co.jp>
2024-07-08 11:22:43 +0700

    fix(Project): Unset unnecessory data before store network into database

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/project/ProjectController.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/project/ProjectController.java
@@ -2930,6 +2930,9 @@ public class ProjectController implements RepresentationModelProcessor<Repositor
 
         releaseNode.setReleaseRelationship(releaseRelationShipUpper);
         releaseNode.setMainlineState(mainLineStateUpper);
+        releaseNode.unsetComponentId();
+        releaseNode.unsetReleaseName();
+        releaseNode.unsetReleaseVersion();
 
         if (operation.equals(ProjectOperation.CREATE)) {
             releaseNode.setCreateOn(SW360Utils.getCreatedOn());
